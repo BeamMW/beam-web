503debb52effee9a74d6e44cdc376b52ffbfb5d3
magicp0tato <131204909+magicp0tato@users.noreply.github.com>
2023-04-21 14:52:42 +0200

    create mining page (wip)

---
+++ pages/mining.vue
@@ -0,0 +1,10 @@
+<template>
+    BEAM MINING
+Similar to many cryptocurrencies, Beam depends on miners to add transactions to its blockchain. While all nodes in the Beam network confirm transaction validity, miners play a crucial role in protecting and securing the network.
+
+Beam employs Beam Hash III, a modified version of the Equihash (150,5) proof-of-work mining algorithm, to ensure network security and stability.
+
+A Comprehensive Guide to Beam Mining:
+
+Dive into the world of Beam mining and learn how to contribute to the network's security and growth with our step-by-step guide.
+</template>
