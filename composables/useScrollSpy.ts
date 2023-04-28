4f49e8ab129dc9801cb8189b083c3efef9b2391c
magicp0tato <131204909+magicp0tato@users.noreply.github.com>
2023-04-28 21:23:39 +0200

    scrollspy improvements for docs

--- composables/useScrollSpy.ts
+++ composables/useScrollSpy.ts
@@ -1,7 +1,45 @@
+import { onMounted, onUnmounted } from "vue";
+
 interface ScrollSpyOptions {
   target: Ref<HTMLElement | null>;
   activeClass: string;
   offset?: number;
+}
+
+function debounce<T extends (...args: any[]) => any, C>(
+  this: C,
+  func: (this: C, ...args: Parameters<T>) => ReturnType<T>,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null;
+
+  return function (this: C, ...args: Parameters<T>): void {
+    const context = this;
+    if (timeout !== null) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  }.bind(this);
+}
+
+function getParentUlFirstLink(
+  linkInLi: HTMLAnchorElement,
+  aside: HTMLElement | null
+): HTMLAnchorElement | null {
+  let currentElement: HTMLElement | null = linkInLi.parentElement;
+
+  while (currentElement && currentElement.tagName !== "UL") {
+    currentElement = currentElement.parentElement;
+  }
+
+  if (currentElement && aside) {
+    const firstLinkInParentUl = aside.querySelector(
+      `a[href="#${currentElement.id}"]`
+    ) as HTMLAnchorElement | null;
+    return firstLinkInParentUl;
+  }
+
+  return null;
 }
 
 export const useScrollSpy =
@@ -15,12 +53,21 @@ export const useScrollSpy =
       h1: null,
       h2: null,
       h3: null,
+      aside: null,
+      article: null,
     };
 
     const observeHeaders = () => {
-      const headers = target.value?.querySelectorAll<HTMLElement>(
-        "article h1, article h2, article h3"
-      );
+      if (!lastIntersectingHeader.aside) {
+        lastIntersectingHeader.aside = target.value?.querySelector("aside");
+      }
+      if (!lastIntersectingHeader.article) {
+        lastIntersectingHeader.article = target.value?.querySelector("article");
+      }
+      const headers =
+        lastIntersectingHeader.article?.querySelectorAll<HTMLElement>(
+          "h1, h2, h3"
+        );
 
       if (!headers) return;
 
@@ -31,15 +78,31 @@ export const useScrollSpy =
             const headerTag: string = (
               entry.target as HTMLElement
             ).tagName.toLowerCase();
-            const link = target.value?.querySelector<HTMLElement>(
-              `aside a[id="link-${id}"]`
-            );
+            const link =
+              lastIntersectingHeader.aside?.querySelector<HTMLElement>(
+                `a[id="link-${id}"]`
+              );
 
             if (entry.isIntersecting) {
               if (headerTag === "h2") {
                 if (lastIntersectingHeader.h3) {
                   lastIntersectingHeader.h3.classList.remove(activeClass);
                   lastIntersectingHeader.h3 = null;
+                }
+              } else if (headerTag === "h3") {
+                if (lastIntersectingHeader.h2) {
+                  const firstLinkInUl = getParentUlFirstLink(
+                    link as HTMLAnchorElement,
+                    lastIntersectingHeader.aside as HTMLElement
+                  );
+                  if (
+                    firstLinkInUl &&
+                    firstLinkInUl !== lastIntersectingHeader.h2
+                  ) {
+                    lastIntersectingHeader.h2.classList.remove(activeClass);
+                    lastIntersectingHeader.h2 = firstLinkInUl;
+                    lastIntersectingHeader.h2?.classList.add(activeClass);
+                  }
                 }
               }
               lastIntersectingHeader[headerTag]?.classList.remove(activeClass);
@@ -57,8 +120,10 @@ export const useScrollSpy =
       headers.forEach((header) => observer?.observe(header));
     };
 
+    const observeHeadersDebounced = debounce(observeHeaders, 100);
+
     onMounted(() => {
-      observeHeaders();
+      requestAnimationFrame(observeHeadersDebounced);
     });
 
     onUnmounted(() => {
