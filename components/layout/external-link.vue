132b58d1b2d11b48caa2daf46735a3c101661665
magicp0tato <131204909+magicp0tato@users.noreply.github.com>
2023-04-29 19:51:25 +0200

    fix: internal and external links icon are now properly handled

--- components/layout/external-link.vue
+++ components/layout/external-link.vue
@@ -1,14 +1,21 @@
 <template>
   <span
-    class="inline-flex items-center hover:opacity-70 transition-opacity text-inherit gap-1"
+    class="inline-flex items-center hover:opacity-70 transition-opacity text-inherit gap-2"
   >
     <span>
       <slot></slot>
     </span>
     <Icon
-      name="layout/external"
-      class="inline-block h-[1.32rem] w-[1.32rem]"
-      v-if="linkType(to) == LinkTypes.EXTERNAL"
+      :name="
+        linkType(to) == LinkTypes.EXTERNAL
+          ? 'layout/external'
+          : 'layout/arrow-right'
+      "
+      :class="{
+        'inline-block': true,
+        'h-[1.32rem] w-[1.32rem]': linkType(to) == LinkTypes.EXTERNAL,
+        'h-4 w-4': linkType(to) == LinkTypes.INTERNAL,
+      }"
     ></Icon>
   </span>
 </template>
