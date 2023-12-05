72d6251525dd2d32f8416fc80fbc4aca91015baa
m4gicpotato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-05 19:41:00 +0000

    feat: add WBEAM info in how to buy (#90)

---
+++ components/buy/notice.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center gap-2">
+    <slot name="icon" />
+    <p
+      class="flex-1 whitespace-wrap break-words text-ellipsis text-xs md:text-sm"
+    >
+      <slot name="text" />
+    </p>
+  </div>
+</template>
