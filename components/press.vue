95d61b76675d7c83925df168b984834c31bf5253
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-07 02:04:17 +0000

    fix: reduce press logos and remove lower padding

--- components/press.vue
+++ components/press.vue
@@ -2,7 +2,7 @@
 const { t } = useI18n();
 </script>
 <template>
-  <LayoutWrapper :center="true" class="pt-10 lg:pt-12 pb-5">
+  <LayoutWrapper :center="true" class="pt-10 lg:pt-12">
     <LayoutTitle :title="t('press.title')" />
     <PressCarousel />
   </LayoutWrapper>
