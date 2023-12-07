f0aa68eaed56315e9b4d53d989f4a9d0923420fe
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-07 01:23:18 +0000

    refactor: use <ClientOnly>

--- components/layout/picture.vue
+++ components/layout/picture.vue
@@ -12,7 +12,6 @@
         type="image/webp"
       />
       <img
-        v-if="clientSide"
         :src="src"
         class="opacity-100 transition-opacity"
         v-bind="$attrs"
@@ -45,10 +44,4 @@ const imageLoaded = ref(false);
 function onImageLoad() {
   imageLoaded.value = true;
 }
-
-const clientSide = ref(false);
-
-onMounted(() => {
-  clientSide.value = true;
-});
 </script>
