92849bf71134d067297d2e412caa2bbe31bc2673
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-07 01:04:32 +0000

    feat: Localized roadmap

--- components/roadmap/carousel.vue
+++ components/roadmap/carousel.vue
@@ -1,207 +1,20 @@
-<script lang="ts" setup>
-import { Swiper, SwiperSlide } from "swiper/vue";
-import SwiperCore from "swiper";
-import { Navigation } from "swiper/modules";
-import { ref, onMounted, onBeforeUnmount } from "vue";
-import { throttle } from "~/utils/throttle";
-
-SwiperCore.use([Navigation]);
-
-enum PROGRESS {
-  PLANNED = "planned",
-  BUILDING = "building",
-  COMPLETED = "completed",
-}
-
-interface RoadmapItem {
-  name: string;
-  year: number;
-  color: string;
-  image: string;
-  types: { description: string; state: PROGRESS }[];
-}
-type Roadmap = RoadmapItem[];
-
-const roadmap: Roadmap = [
-  {
-    name: "Agile Atom",
-    year: 2019,
-    color: "#00f3d0",
-    image: "atom",
-    types: [
-      { description: "Payment and Exchange API", state: PROGRESS.COMPLETED },
-      { description: "Mining Pool API", state: PROGRESS.COMPLETED },
-      { description: "Lightning Position Paper", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Bright Boson",
-    year: 2019,
-    color: "#00d3ff",
-    image: "boson",
-    types: [
-      { description: "Android and iOS Wallets", state: PROGRESS.COMPLETED },
-      {
-        description: "Payment Platforms Integration",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "Fast Node sync", state: PROGRESS.COMPLETED },
-      { description: "Payment Confirmation", state: PROGRESS.COMPLETED },
-      { description: "Beam Anywhere POC", state: PROGRESS.COMPLETED },
-      { description: "Cold Wallet", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Clear Cathode",
-    year: 2019,
-    color: "#3d9be9",
-    image: "cathode",
-    types: [
-      {
-        description: "Beam ↔ BTC, LTC, QTUM Atomic Swap (Desktop wallet)",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "PoW Algorithm Change", state: PROGRESS.COMPLETED },
-      { description: "Lightning POC", state: PROGRESS.COMPLETED },
-      { description: "Lelantus-MW POC", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Double Doppler",
-    year: 2020,
-    color: "#9d6eff",
-    image: "doppler",
-    types: [
-      {
-        description: "Atomic Swaps Marketplace (Desktop wallet) and API",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "Laser Beam", state: PROGRESS.COMPLETED },
-      { description: "Lelantus Testnet", state: PROGRESS.COMPLETED },
-      { description: "Wallet Update notifications", state: PROGRESS.COMPLETED },
-      { description: "Wallet fiat value indicator", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Eager Electron",
-    year: 2020,
-    color: "#f86cff",
-    image: "electron",
-    types: [
-      {
-        description: "Confidential Assets on Mainnet",
-        state: PROGRESS.COMPLETED,
-      },
-      {
-        description: "BeamHashIII PoW mining algorithm",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "Lelantus MW", state: PROGRESS.COMPLETED },
-      { description: "One-side payment", state: PROGRESS.COMPLETED },
-      { description: "Sync balance over same seed", state: PROGRESS.COMPLETED },
-      { description: "Mobile Node", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Fierce Fermion",
-    year: 2022,
-    color: "#b02fb0",
-    image: "fermion",
-    types: [
-      {
-        description: "Beam Virtual Machine in wallet and node",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "In-wallet DAPP store", state: PROGRESS.COMPLETED },
-    ],
-  },
-  {
-    name: "Groovy Gluon",
-    year: 2022,
-    color: "#bc1b9f",
-    image: "gluon",
-    types: [
-      { description: "IPFS Integration", state: PROGRESS.COMPLETED },
-      { description: "Decentralized DApp Store", state: PROGRESS.COMPLETED },
-      { description: "High Frequency Transactions", state: PROGRESS.COMPLETED },
-      {
-        description: "DAO core DAPP (governance and voting)",
-        state: PROGRESS.COMPLETED,
-      },
-      { description: "Asset Swap Marketplace", state: PROGRESS.COMPLETED },
-      { description: "Blockchain Explorer Updates", state: PROGRESS.COMPLETED },
-      { description: "Shader SDK", state: PROGRESS.COMPLETED },
-      { description: "Bridges release", state: PROGRESS.COMPLETED },
-      { description: "Oracles", state: PROGRESS.COMPLETED },
-      { description: "AMM DEX release", state: PROGRESS.COMPLETED },
-      {
-        description: "HW Wallet Support (Ledger Nano S & Nano S Plus)",
-        state: PROGRESS.COMPLETED,
-      },
-      {
-        description: "Beam Messenger DApp in Beam Wallet",
-        state: PROGRESS.COMPLETED,
-      },
-    ],
-  },
-  {
-    name: "Hearty Hadron and Later",
-    year: 2023,
-    color: "#c8502d",
-    image: "hadron",
-    types: [
-      { description: "EVM support", state: PROGRESS.BUILDING },
-      { description: "Hard Fork 8.0", state: PROGRESS.PLANNED },
-      { description: "Sidechains support", state: PROGRESS.PLANNED },
-      {
-        description: "Launching EVM DApps on Beam chain",
-        state: PROGRESS.PLANNED,
-      },
-    ],
-  },
-].reverse();
-
-const slidesToShow = ref(1);
-const customNextButton = ref<HTMLElement | null>(null);
-const customPrevButton = ref<HTMLElement | null>(null);
-
-const calculateSlidesToShow = () => {
-  if (process.client) {
-    const width = window.innerWidth;
-    if (width >= 1280) {
-      return 3;
-    } else if (width >= 1024) {
-      return 2;
-    }
-  }
-  return 1;
-};
-
-const updateSlidesToShow = throttle(() => {
-  slidesToShow.value = calculateSlidesToShow();
-}, 100);
-
-onMounted(() => {
-  window.addEventListener("resize", updateSlidesToShow);
-  updateSlidesToShow();
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener("resize", updateSlidesToShow);
-});
-</script>
-
 <template>
   <div class="w-full relative">
-    <div ref="customNextButton" class="group custom-swipe-arrow next-button">
+    <div
+      ref="customNextButton"
+      class="custom-next-button group absolute right-0 z-10 cursor-pointer bg-gradient-to-l from-[#041D3C] h-full w-24 lg:w-20 flex items-center justify-end"
+    >
       <Icon
-        class="block w-5 h-5 md:w-7 md:h-7 text-white group-hover:opacity-100 opacity-60 transition-opacity"
+        class="block w-7 h-7 text-white group-hover:opacity-100 opacity-60 transition-opacity"
         name="layout/arrow-right"
       />
     </div>
-    <div ref="customPrevButton" class="group custom-swipe-arrow prev-button">
+    <div
+      ref="customPrevButton"
+      class="custom-prev-button group absolute left-0 z-10 cursor-pointer bg-gradient-to-r from-[#041D3C] h-full w-20 flex items-center justify-start"
+    >
       <Icon
-        class="block rotate-180 w-5 h-5 md:w-7 md:h-7 text-white group-hover:opacity-100 opacity-60 transition-opacity"
+        class="block rotate-180 w-7 h-7 text-white group-hover:opacity-100 opacity-60 transition-opacity"
         name="layout/arrow-right"
       />
     </div>
@@ -276,3 +89,294 @@ onBeforeUnmount(() => {
     </swiper>
   </div>
 </template>
+
+<script lang="ts" setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+import { throttle } from "~/utils/throttle";
+
+const { t } = useI18n();
+
+SwiperCore.use([Navigation]);
+
+enum PROGRESS {
+  PLANNED = "planned",
+  BUILDING = "building",
+  COMPLETED = "completed",
+}
+
+interface RoadmapItem {
+  name: string;
+  year: number;
+  color: string;
+  image: string;
+  types: { description: string; state: PROGRESS }[];
+}
+type Roadmap = RoadmapItem[];
+
+const roadmap: Roadmap = [
+  {
+    name: t("roadmap.milestones.1.title"),
+    year: 2019,
+    color: "#00f3d0",
+    image: "atom",
+    types: [
+      {
+        description: t("roadmap.milestones.1.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.1.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.1.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.2.title"),
+    year: 2019,
+    color: "#00d3ff",
+    image: "boson",
+    types: [
+      {
+        description: t("roadmap.milestones.2.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.2.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.2.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.2.items.4"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.2.items.5"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.2.items.6"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.3.title"),
+    year: 2019,
+    color: "#3d9be9",
+    image: "cathode",
+    types: [
+      {
+        description: t("roadmap.milestones.3.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.3.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.3.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.3.items.4"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.4.title"),
+    year: 2020,
+    color: "#9d6eff",
+    image: "doppler",
+    types: [
+      {
+        description: t("roadmap.milestones.4.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.4.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.4.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.4.items.4"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.4.items.5"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.5.title"),
+    year: 2020,
+    color: "#f86cff",
+    image: "electron",
+    types: [
+      {
+        description: t("roadmap.milestones.5.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.5.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.5.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.5.items.4"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.5.items.5"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.5.items.6"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.6.title"),
+    year: 2022,
+    color: "#b02fb0",
+    image: "fermion",
+    types: [
+      {
+        description: t("roadmap.milestones.6.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.6.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.7.title"),
+    year: 2022,
+    color: "#bc1b9f",
+    image: "gluon",
+    types: [
+      {
+        description: t("roadmap.milestones.7.items.1"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.2"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.3"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.4"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.5"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.6"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.7"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.8"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.9"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.10"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.11"),
+        state: PROGRESS.COMPLETED,
+      },
+      {
+        description: t("roadmap.milestones.7.items.12"),
+        state: PROGRESS.COMPLETED,
+      },
+    ],
+  },
+  {
+    name: t("roadmap.milestones.8.title"),
+    year: 2023,
+    color: "#c8502d",
+    image: "hadron",
+    types: [
+      {
+        description: t("roadmap.milestones.8.items.1"),
+        state: PROGRESS.BUILDING,
+      },
+      {
+        description: t("roadmap.milestones.8.items.2"),
+        state: PROGRESS.PLANNED,
+      },
+      {
+        description: t("roadmap.milestones.8.items.3"),
+        state: PROGRESS.PLANNED,
+      },
+      {
+        description: t("roadmap.milestones.8.items.4"),
+        state: PROGRESS.PLANNED,
+      },
+    ],
+  },
+].reverse();
+
+const slidesToShow = ref(1);
+const customNextButton = ref(null);
+const customPrevButton = ref(null);
+
+const calculateSlidesToShow = () => {
+  if (process.client) {
+    const width = window.innerWidth;
+    if (width >= 1280) {
+      return 3;
+    } else if (width >= 1024) {
+      return 2;
+    }
+  }
+  return 1;
+};
+
+const updateSlidesToShow = throttle(() => {
+  slidesToShow.value = calculateSlidesToShow();
+}, 100);
+
+onMounted(() => {
+  window.addEventListener("resize", updateSlidesToShow);
+  updateSlidesToShow();
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", updateSlidesToShow);
+});
+</script>
