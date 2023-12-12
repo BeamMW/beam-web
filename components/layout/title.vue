d783c5e2c348ecc6ebcd4a5f9dca2f436024c7ce
m4gicpotato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-12 20:56:51 +0000

    feat: Download started banner (#129)

--- components/layout/title.vue
+++ components/layout/title.vue
@@ -8,7 +8,7 @@ defineProps({
 </script>
 <template>
   <h2
-    class="text-center w-full pb-8 lg:pb-10 text-2xl uppercase font-bold tracking-[.25em]"
+    class="text-center max-w-5xl mx-auto px-4 md:px-0 pb-8 lg:pb-10 text-xl md:text-2xl uppercase font-bold tracking-[.25em]"
   >
     {{ title }}
   </h2>
