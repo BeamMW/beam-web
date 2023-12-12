b966ef9fe434544113e7d25184d0507542bba8c6
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-12 07:47:06 +0000

    add dex to prerender and prefetch on top of preconnect

--- app.config.ts
+++ app.config.ts
@@ -80,8 +80,8 @@ export const preHtml = {
     new URL(ExternalLinks.DEX).origin,
   ] as URLEntry[],
   "dns-prefetch": extractOriginsFromEnum(ExternalLinks) as URLEntry[],
-  prerender: [] as URLEntry[],
-  prefetch: [] as URLEntry[],
+  prerender: [new URL(ExternalLinks.DEX).origin] as URLEntry[],
+  prefetch: [new URL(ExternalLinks.DEX).origin] as URLEntry[],
 };
 
 export const docTypes = [
