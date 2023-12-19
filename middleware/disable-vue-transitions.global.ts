39f240e97ce7829aa4e2ee6d9b5af84b7ac71d11
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-19 00:18:21 +0000

    improve transitions

---
+++ middleware/disable-vue-transitions.global.ts
@@ -0,0 +1,9 @@
+export default defineNuxtRouteMiddleware((to) => {
+  if (import.meta.server || !document.startViewTransition) {
+    return;
+  }
+
+  // Disable built-in Vue transitions
+  to.meta.pageTransition = false;
+  to.meta.layoutTransition = false;
+});
