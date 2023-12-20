74e5585c3f3c7b9dd56d9c21aa46bfe3ea1e7f25
magicp0tato <131204909+m4gicpotato@users.noreply.github.com>
2023-12-20 15:38:46 +0000

    refactor: enhance resources pages

--- components/mining/resources.vue
+++ components/mining/resources.vue
@@ -1,32 +1,33 @@
 <script lang="ts" setup>
-import { ExternalLinks } from "~/app.config";
-
-const { t } = useI18n();
-
-const styles =
-  "bg-[#012a08]/50 hover:bg-[#012a08] focus:bg-[#012a08] focus:ring-offset-[#012a08] focus:ring-beam-green-dark text-beam-green-dark";
+import { ExternalLinks, ExternalLinksTitle } from "~/app.config";
 </script>
 <template>
   <LayoutWrapper :center="true">
     <LayoutTitle
-      :title="t('downloads.resources.title')"
+      :title="$t('downloads.resources.title')"
       gradient-color="specialGradientGreen"
     />
     <div class="grid lg:grid-cols-2 items-center gap-6 md:gap-10 text-white">
       <ResourcesLink
-        :title="t('mining.resources.calculator')"
+        :title="$t('mining.resources.calculator')"
         image="mining/resources/mining"
         :link="ExternalLinks.MINING_CALCULATOR"
         link-text="WhatToMine"
-        :class="styles"
+        class="link-resources"
       />
       <ResourcesLink
-        :title="t('mining.resources.support')"
+        :title="$t('mining.resources.support')"
         image="mining/resources/help"
         :link="ExternalLinks.MINING_SUPPORT"
-        link-text="Telegram"
-        :class="styles"
+        :link-text="ExternalLinksTitle.TELEGRAM"
+        class="link-resources"
       />
     </div>
   </LayoutWrapper>
 </template>
+
+<style scoped>
+.link-resources {
+  @apply bg-[#012a08]/50 hover:bg-[#012a08] focus:bg-[#012a08] focus:ring-offset-[#012a08] focus:ring-beam-green-dark text-beam-green-dark;
+}
+</style>
